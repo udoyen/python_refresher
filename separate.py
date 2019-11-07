d5b7a6ef1dc18cd30423b63ec253592b5ee23305
udoyen <datameshprojects@gmail.com>
2019-11-07 21:04:46 +0100

    done

--- separate.py
+++ separate.py
@@ -1,1 +1,14 @@
-sentence = 'I was here universe, galaxy, world'
+import re
+
+sentence = 'I was here | {universe, galaxy, world}'
+# print(sentence.split('|', 2)[1])
+collect = sentence.split('|', 2)[1]
+
+dsrc = re.findall(r'\w+', collect)
+
+sentb = sentence.split('|', 2)[0]
+print(sentb)
+
+for i in range(0, 3):
+    print(sentb + dsrc[i])
+    # print(i)
