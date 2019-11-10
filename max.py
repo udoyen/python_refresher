44a8feed2eea6442a20fc2534a70cf3e0d8b3280
udoyen <datameshprojects@gmail.com>
2019-11-11 00:05:31 +0100

    updated the mymax function

--- max.py
+++ max.py
@@ -1,15 +1,22 @@
 fam = [2, 5, 3, 20, 4, 12, 100]
+fam2 = [-0.3, -0.2, -0.5, -0.95, -0.9]
 gin = ()
 
+# @profile
 def mymax(ls):
     n = 0
+    zero_status = False
+    state = False
     if isinstance(ls, list):
-        for i in range(0, len(ls)):
-            for i in ls:
-                if i > n:
-                    n = i
+        for i in ls:
+            if i <= n and state is False:
+                n = i
+            if i > n:
+                state = True
+                n = i
         print(n)
     else:
         print("The passed in argument is not a list")
 
-mymax(fam)
+if __name__ == "__main__":
+    mymax(fam)
