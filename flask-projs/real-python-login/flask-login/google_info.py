f6d9d3c5e9052856e6d244e5fbaf85bcd3656397
udoyen <datameshprojects@gmail.com>
2019-11-14 23:26:49 +0100

    finished tutorial

--- flask-projs/real-python-login/flask-login/google_info.py
+++ flask-projs/real-python-login/flask-login/google_info.py
@@ -2,6 +2,6 @@ myinfo = {
 
     "GOOGLE_CLIENT_ID": '206185119995-mkd9jga83kntr88gpginj4j21pdgdbap.apps.googleusercontent.com',
 
-    "GOOGLE_CLIENT_SECRETE_URL": 'CEb5UU300eHmMffsjXRD2YNa'
+    "GOOGLE_CLIENT_SECRETE": 'CEb5UU300eHmMffsjXRD2YNa'
 
 }
