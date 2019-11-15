bf8b83982f6fe7ec64e38c7def433f00204b296b
udoyen <datameshprojects@gmail.com>
2019-11-15 02:25:48 +0100

    done with sum implementation

--- sum.py
+++ sum.py
@@ -1,9 +1,15 @@
 list = [3, 3, 2, 1, 0, 9, 8]
+name = ['george', 'udosen']
 
 def d_sum(some_list):
-    r = 0
+    r = None    
+    for j in some_list:
+        if isinstance(j, str):
+            r = ""
+        else:
+            r = 0
     for i in some_list:
         r += i
     print(r)
 
-d_sum(list)
+d_sum(name)
