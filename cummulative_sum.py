f8a32ebbc7aa97d88f023dc5940a3cb3cb399968
udoyen <datameshprojects@gmail.com>
2019-11-16 19:18:05 +0100

    cummulaive product done

--- cummulative_sum.py
+++ cummulative_sum.py
@@ -1,8 +1,16 @@
 s = ['a', 'b', 'c', 'd', 'e', 'f']
+t = [1, 2, 3, 4]
+u = [4, 3, 2, 1]
 
 def cummulative_sum(n):
     for i in range(len(n) - 1):
         n[i + 1] += n[i]
-    print(n)    
+    print(n)
 
-cummulative_sum(s)
+def cummulative_prod(n):
+    for i in range(len(n) - 1):
+        n[i + 1] *= n[i]
+    print(n)
+
+# cummulative_sum(t)
+cummulative_prod(s)
