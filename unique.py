f577cf32d4f7e4969995e57cc2147b8956a48718
udoyen <datameshprojects@gmail.com>
2019-11-17 18:02:43 +0100

    unique done

--- unique.py
+++ unique.py
@@ -1,18 +1,21 @@
 f = [1, 2, 1, 3, 2, 5]
+s = ["python", "java", "Python", "Java"]
 
-def unique(n):
+def unique(n, key=lambda s: s.lower()):
     new = []
-    for i in range(len(n)):
-        d = n.pop(0)        
-        if d in n:
-            new.append(d)
-            n.remove(d)
-        else:
-            new.append(d)
-        if len(n) == 0:
-            break
-
+    for a in n:
+        if isinstance(a, str):
+            n = list(map(key, n))
+        for i in range(len(n)):
+            d = n.pop(0)        
+            if d in n:
+                new.append(d)
+                n.remove(d)
+            else:
+                new.append(d)
+            if len(n) == 0:
+                break
     print(new)
 
 
-unique(f)
+unique(s)
