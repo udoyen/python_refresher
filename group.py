1bd62e24af166d5abacc7e798b98bb2c4bc2b42f
udoyen <datameshprojects@gmail.com>
2019-11-17 10:56:02 +0100

    group function done

---
+++ group.py
@@ -0,0 +1,29 @@
+f = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+g = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def group(ls, n):
+    mother = []
+    div = int(len(ls) / n)
+    rem = len(ls) % n
+    if isinstance(ls, list) and isinstance(n, int):
+        if len(ls) >= n:
+            for i in range(div):
+                child = []
+                for j in range(n):
+                    for l in ls:
+                        child.append(ls.pop(0))
+                        break
+                        
+                mother.append(child)
+            if rem > 0:
+                child = []
+                for k in range(rem):
+                    child.append(ls.pop())
+                mother.append(child)
+        else:
+            print('List length should be grester than the divisor')
+        print(mother)
+    else:
+        print('Wrong arguments! ls = List and n = int required')
+
+group(f, 3)
