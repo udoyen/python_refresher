2c57f802ff8df65067911ae2767bc2c8d2abfae8
udoyen <datameshprojects@gmail.com>
2019-11-18 10:34:15 +0100

    unique function fixed

--- python_builtins/unique.py
+++ python_builtins/unique.py
@@ -1,20 +1,17 @@
 f = [1, 2, 1, 3, 2, 5]
-s = ["python", "java", "Python", "Java"]
+s = ["python", "java", "Python", "Java", "java", "google", "Python", "Google", "gOOGLE", "google", "JAVA", "JavA"]
+
 
 def unique(n, key=lambda s: s.lower()):
     new = []
+    if isinstance(n, list):
+        n = list(map(key, n))
     for a in n:
-        if isinstance(a, str):
-            n = list(map(key, n))
-        for i in range(len(n)):
-            d = n.pop(0)        
-            if d in n:
-                new.append(d)
-                n.remove(d)
-            else:
-                new.append(d)
-            if len(n) == 0:
-                break
+        d = n.pop(0)
+        new.append(d)
+        for j in n:
+            if j == d:
+                n.remove(j)
     print(new)
 
 
