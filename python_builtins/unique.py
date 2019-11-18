95429467ec0f99412947e2c90e841544109782cb
udoyen <datameshprojects@gmail.com>
2019-11-18 11:01:45 +0100

    unique for numbers fixed

--- python_builtins/unique.py
+++ python_builtins/unique.py
@@ -1,18 +1,32 @@
-f = [1, 2, 1, 3, 2, 5]
+f = [1, 2, 1, 3, 2, 5, 6, 5, 5, 4, 4, 4, 4, 10, 10, 11, 12, 13, 14, 6, 5]
 s = ["python", "java", "Python", "Java", "java", "google", "Python", "Google", "gOOGLE", "google", "JAVA", "JavA"]
 
 
 def unique(n, key=lambda s: s.lower()):
     new = []
-    if isinstance(n, list):
-        n = list(map(key, n))
-    for a in n:
-        d = n.pop(0)
-        new.append(d)
-        for j in n:
-            if j == d:
-                n.remove(j)
+    state = False
+    for k in n:
+        if isinstance(k, str):
+            n = list(map(key, n))
+            state = True
+    if state:
+        for a in n:
+            d = n.pop(0)
+            new.append(d)
+            for j in n:
+                if j == d:
+                    n.remove(j)
+    else:
+        for a in range(len(n)):
+            if len(n) == 0:
+                break
+            d = n.pop(0)
+            if d not in new:
+                new.append(d)
+            for j in n:
+                if j == d:
+                    n.remove(j)
     print(new)
 
 
-unique(s)
+unique(f)
